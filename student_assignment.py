7f53ae2770e1b7831c2002055aaeaf406ae9a270
Roy2 Huang <roy2_huang@asus.com>
2025-01-23 17:12:04 +0800

    HW2_1 : version 2

--- student_assignment.py
+++ student_assignment.py
@@ -2,12 +2,26 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import (CharacterTextSplitter,
                                       RecursiveCharacterTextSplitter)
 
+import os
+
 q1_pdf = "OpenSourceLicenses.pdf"
 q2_pdf = "勞動基準法.pdf"
 
 
 def hw02_1(q1_pdf):
-    pass
+    pdf_path = os.getcwd() + os.sep + q1_pdf
+    if os.path.isfile(pdf_path):
+        pdf_loader = PyPDFLoader(pdf_path)
+        documents = pdf_loader.load()
+        text_splitter = CharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=0
+        )
+        split_documents = text_splitter.split_documents(documents)
+        return split_documents[-1]
+    else:
+        print("Invalid file path")
+        return
 
 def hw02_2(q2_pdf):
     pass
